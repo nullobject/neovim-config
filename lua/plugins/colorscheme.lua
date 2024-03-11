aa42fa8d3a01b517236338d74a6423b76ba0b20c
Joshua Bassett <hello@joshbassett.info>
2024-03-12 10:20:32 +1100

    Refactor colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,14 @@
 return {
-  { "rose-pine/neovim", name = "rose-pine", opts = { dark_variant = "moon" } },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      dark_variant = "moon",
+      styles = { transparency = false },
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = { colorscheme = "rose-pine" },
   },
-  -- { "Mofiqul/dracula.nvim" },
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = { colorscheme = "dracula" },
-  -- },
 }
