484193405007b2620185e6ffdcea210f281b2464
Joshua Bassett <hello@joshbassett.info>
2024-03-11 16:37:06 +1100

    Combine haskell and treesitter plugins

--- lua/plugins/haskell.lua
+++ lua/plugins/haskell.lua
@@ -4,4 +4,10 @@ return {
     version = "^3",
     ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "haskell" })
+    end,
+  },
 }
