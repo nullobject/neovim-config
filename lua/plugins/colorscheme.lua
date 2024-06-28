a0f5cb34d9d2b5a83692ca2e0e65e5b24b02cc7f
Joshua Bassett <hello@joshbassett.info>
2024-06-28 10:24:41 +1000

    Use opaque background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
     name = "rose-pine",
     opts = {
       dark_variant = "moon",
-      styles = { transparency = true },
+      styles = { transparency = false },
     },
   },
   { "catppuccin/nvim", lazy = false, priority = 1000 },
